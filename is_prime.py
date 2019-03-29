b9984310453a135189076e943a6207946551aff7
ARKAROK <henry.nunez.2019@upb.edu.co>
2019-03-29 14:24:24 -0500

    Primera modificacion tercer punto

--- is_prime.py
+++ is_prime.py
@@ -1,19 +1,23 @@
 print("Funcion primo")
+while True:
+	n=int(input("Digita un numero: "))
+	if n<=0:
+		print("Numeros no contemplados")
+		break
 
-n=int(input("Digita un numero: "))
+	def is_prime():
+		try:
+			cont=0
+			for i in range(1,n+1):
+				if n%i==0:
+					cont+=1
+			if cont>2:
+				print("0")
+			else:
+				print("1")
+		except:
+			print("-1")
 
-def is_prime():
-	cont=0
-	for i in range(1,n+1):
-		if n%i==0:
-			cont+=1
-	if cont>2:
-		print("Is NOT a prime number")
-	else:
-		print("Is a prime number")
-
-is_prime()
+	is_prime()
 
 print("Final del programa")
-
-			
