c81ebf157d6182c7be713df4c4b006a5856d8f93
Nunez Suarez <000400886@bga.upb>
2019-03-29 11:53:15 -0500

    Segunda modificacion

--- a_power_b.py.py
+++ a_power_b.py.py
@@ -1,19 +1,33 @@
-print("Algoritmo potencia")
+
+def potencia(a,b):
+  pro=1
+  
+  for i in range(1,b):
+    if i>100:
+      raise(Exception("Programa muy amplio"))
+    pro=pro*a  
+  print(pro)  
+  
+  return pro
+  
+print("Algorito potencia")  
+
+bug=0
 
 while True:
-  a=int(input("Digita un numero: "))
-  if a==0:
-    print("El valor 0 no esta contemplado")
-    break
-  
-  b=int(input("Digita otro numero: "))
-  def potencia(a,b):
-      pro=a
-      for i in range(1,b):
-        pro=pro*a
-        
-      print(pro)
+  try:
     
-  potencia(a,b)
-  
+    a=int(input("Digita un numero: "))
+    
+    if a==0:
+      break
+    
+    B=int(input("Digita un numero: "))
+    
+    prod=potencia(a,b)
+    print(prod)
+  except:
+    print("Hay un error en el sistema :'v")
+    bug+=1
+    
 print("Final del programa")
